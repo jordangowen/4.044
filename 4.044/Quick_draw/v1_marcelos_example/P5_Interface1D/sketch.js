17cc99163ca48196ac8367ba5d01a425f0062013
Jordan Gowen <jordan@jordangowen.com>
2021-03-02 08:55:13 -0500

    Moved Quick_Draw

---
+++ 4.044/Quick_draw/v1_marcelos_example/P5_Interface1D/sketch.js
@@ -0,0 +1,74 @@
+/* /////////////////////////////////////
+
+  4.043 / 4.044 Design Studio: Advanced Interactions
+  February  18, 2021
+  Marcelo Coelho
+
+  If you come from Processing, there is a Processing version here. 
+  Keep in mind that there are some differences between both versions:
+  https://github.com/marcelocoelho/Interface1D
+
+*/ /////////////////////////////////////
+
+
+let displaySize = 30;   // how many pixels are visible in the game
+let pixelSize = 20;     // how big should they look on screen
+
+let playerOne;    // Adding 2 players to the game
+let playerTwo;
+let playerThree;
+
+let target;       // add one target to left of screen.
+let targetTwo;    // one to the right of the screen
+
+let display;      // Aggregates our final visual output before showing it on the screen
+
+let controller;   // This is where the state machine and software logic lives
+
+let collisionAnimation;   // Where we store and manage the collision animation
+
+let score;        // Where we keep track of score and winner
+
+
+
+function setup() {
+
+  createCanvas((displaySize*pixelSize), pixelSize);     // dynamically sets canvas size
+
+  display = new Display(displaySize, pixelSize);        //Initializing the display
+
+  // playerOne = new Player(color(255,0,0), parseInt(random(0,displaySize)), displaySize);   // Initializing players
+  // playerTwo = new Player(color(0,0,255), parseInt(random(0,displaySize)), displaySize);
+
+  playerOne = new Player(color(255,0,0), 14, displaySize);   // Initializing players at CENTER
+  playerTwo = new Player(color(0,0,255), 15, displaySize);
+  // playerThree = new Player(color(0,255,0), 13, displaySize);
+
+
+  target = new Player(color(255,255,0), 0, displaySize);    // Initializing target using the Player class 
+  targetTwo = new Player(color(255,255,0), 29, displaySize);      // Initializing second target called 
+  // targetTwo = new Player(color(255,255,0), 20, displaySize);    // Initializing target using the Player class 
+
+  collisionAnimation = new Animation();     // Initializing animation
+
+  controller = new Controller();            // Initializing controller
+
+  score = {max:3, winner:color(0,0,0)};     // score stores max number of points, and color 
+
+}
+
+function draw() {
+
+  // start with a blank screen
+  background(0, 0, 0);    
+
+  // Runs state machine at determined framerate
+  controller.update();
+
+  // After we've updated our states, we show the current one 
+  display.show();
+
+
+}
+
+
